f1f9dbaf9b781da2fd87071eaa6f33cfc55b8d36
Camila Canali <camicanali@gmail.com>
2015-04-17 17:31:48 -0300

    Aula 17/04/2015

---
+++ src/Lab1.sql
@@ -0,0 +1,49 @@
+/*
+  Copyright (c) 2011, CWI Software. Todos os direitos.
+  Projeto Crescer - Treinamento de banco de dados 
+  ---
+  Laboratório 1: Comandos DDL, criação de tabelas e constraints.  
+*/
+
+Create table Cidade 
+(
+  IDCidade  int         not null,
+  Nome      varchar(30) not null,
+  UF        varchar(2)  not null,
+    constraint PK_Cidade primary key (IDCidade)
+);	
+
+Create table Associado 
+(
+  IDAssociado    int         not null,
+  Nome           varchar(50) not null,
+  DataNascimento datetime    not null,
+  Sexo           char(1)     not null,
+  CPF            varchar(11) ,
+  Endereco       varchar(35),
+  Bairro         varchar(25),
+  Complemento    varchar(20),
+  IDCidade       int,
+     constraint PK_Associado primary key (IDAssociado),
+	 constraint FK_Associado foreign key (IDCidade)
+	    references Cidade (IDCidade)
+);
+
+select * from associado;
+
+select *
+into CopiaCidade
+from Cidade;
+
+select * from CopiaCidade;
+
+BEGIN TRANSACTION
+select * from Cidade where IDCidade = 1;
+update Cidade
+set Nome = 'Sao Leopoldo'
+where IDCidade = 1;
+select * from Cidade
+rollback
+
+Truncate table CopiaCidade;
+
