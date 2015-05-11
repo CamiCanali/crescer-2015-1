eb6c253e42d55217525e6d0b54debcb3e0656f20
Camila Canali <camicanali@gmail.com>
2015-05-10 21:26:24 -0300

    Exercício dos times de futebol

--- js/futebol.js
+++ js/futebol.js
@@ -1,4 +1,5 @@
-
+<html>
+<script>
 var chelsea = {
   nome: 'Chelsea Football Club',
   titulos: [
@@ -37,20 +38,63 @@ var manchester= {
 
 var clubes = [chelsea, arsenal, liverpool, manchester];
 
-var soma = 0;
-
-function somarNacional(i){
-	soma = soma + i.titulos[0].qtd;
+//ordena clubes por titulos e por índice
+function ordenaTitulos(array, i){
+  return(array.sort(function (a,b){
+    return a.titulos[i].qtd < b.titulos[i].qtd;
+  }));
 }
-//ordenada por nacionais-
-clubes.sort(function (a,b){
-	return a.titulos[0].qtd < b.titulos[0].qtd});
-//ordenada por continentais
-clubes.sort(function (a,b){
-	return a.titulos[1].qtd < b.titulos[1].qtd});
-//ordenada por mundiais
-clubes.sort(function (a,b){
-	return a.titulos[2].qtd < b.titulos[2].qtd});	
-	
-//somar por nacionais
-clubes.map(somar())
+
+//ordena array de clubes por nacionais
+var ordenaPorNacionais = function (clubes){
+  return ordenaTitulos(clubes, 0);
+}
+//ordena array de clubes por continentais
+var ordenaPorContinentais = function (clubes){
+  return ordenaTitulos(clubes, 1);
+}
+//ordena array de clubes por nacionais
+var ordenaPorMundiais = function (clubes){
+  return ordenaTitulos(clubes, 0);
+}
+
+//soma titulos dos clubes por índice
+function somarTitulos(array, i){
+  var soma = 0;
+  array.map(function(a){
+    soma = soma + a.titulos[i].qtd;
+  });
+  return soma;
+}
+//soma os titulos de todos os clubes por nacionais
+var somaNacional = function(clubes){
+  return somarTitulos(clubes,0);
+}
+//soma os titulos de todos os clubes por continentais
+var somaContinental = function(clubes){
+  return somarTitulos(clubes,1);
+}
+//soma os titulos de todos os clubes por mundiais
+var somaTodos = function(clubes){
+  return somarTitulos(clubes,0) + somarTitulos(clubes,1) + somarTitulos(clubes,2);
+}
+//função que percorre o arrray de clubes
+//e retorna os que possuem título nacional >18
+function filtroMais18(array){
+  return array.titulos[0].qtd > 18;
+}
+//chama o filtroMais18
+var apenasOsMelhores = function(array){
+  return array.filter(function (a){
+    return filtroMais18(a);
+  });
+}
+//retorna tipo JSON JavaScript Object Notation
+var apenasOsMelhoresParaJSON = function (array){
+  return JSON.stringify(apenasOsMelhores(array));
+}
+var apenasOsMelhoresRecebdnoJSON = function(String){
+  return JSON.parse(String);
+}
+</script>
+</html>
