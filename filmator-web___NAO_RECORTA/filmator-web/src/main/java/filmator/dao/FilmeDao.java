570bb3db759731707cdadb569e3b7ac4a1944953
Camila Canali <camicanali@gmail.com>
2015-05-26 13:35:15 -0300

    filmator web atualização

--- filmator-web___NAO_RECORTA/filmator-web/src/main/java/filmator/dao/FilmeDao.java
+++ filmator-web___NAO_RECORTA/filmator-web/src/main/java/filmator/dao/FilmeDao.java
@@ -1,14 +1,17 @@
 package filmator.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import filmator.model.Filme;
+import filmator.model.Genero;
 
 @Component
 public class FilmeDao {
@@ -18,13 +21,24 @@ public class FilmeDao {
 	
 	
 	public void inserir(Filme filme){
-		jdbcTemplate.update("INSERT INTO Filme (nome) VALUES (?)", filme.getNome());
+		jdbcTemplate.update("INSERT INTO Filme (nome, genero, ano, imagem) VALUES (?,?,?,?)", filme.getNome(), 
+				filme.getGenero(),filme.getAno(), filme.getImagem());
 	}
 	
 	public List<Filme> buscaTodosFilmesJava8(){
-		return jdbcTemplate.query("SELECT nome FROM Filme", (ResultSet rs, int rowNum) -> {	
-			Filme filme = new Filme(rs.getString("nome"));
-			return filme;
+		return jdbcTemplate.query("SELECT nome, genero, ano, imagem FROM Filme", new RowMapper<Filme>(){	
+			//Filme filme = new Filme(rs.getString("nome"));
+			//return filme;
+			
+				@Override
+				public Filme mapRow(ResultSet rs, int arg) throws SQLException {
+					Filme filme = new Filme();
+					filme.setNome(rs.getString("nome"));
+					filme.setAno(rs.getInt("ano"));
+					filme.setGenero(Genero.valueOf(rs.getString("genero")));
+					filme.setImagem(rs.getString("imagem"));			
+					return filme;
+				}			
 		});	
 	}
 }
